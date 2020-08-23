ae526d9f0450b68590b75b14349763efed0fc12b
Sanny67 <sayantanichatterjee67@gmail.com>
2020-08-23 14:29:45 +0530

    after adding all app routes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -27,6 +27,62 @@ class AdminController extends Controller
         return view('admin');
     }
 
+    public function regapp(Request $request){
+
+        $this->validate($request, [
+            'name' => 'required|regex:/^[A-Za-z ]+$/',
+            'phone' => 'required|min:8|max:10|unique:admins|regex:/^[0-9]+$/',
+            'email' => 'required|email|unique:admins',
+            'password' => 'required|regex:/^[^\W_]+$/|min:8|max:13',
+        ]);
+
+        $name = $request->input('name');
+        $phone = $request->input('phone');
+        $email = $request->input('email');
+        $pass = $request->input('password');
+        $password = (new BcryptHasher)->make($request->input('password'));
+
+        $admin = new Admin([
+            'name' => $request->input('name'),
+            'phone' => $request->input('phone'),
+            'email' => $request->input('email'),
+            'password' => $password,
+            'status' => 1,
+            'api_token' => Str::random(60),
+        ]);
+
+        $admin->save();
+        return response()->json(['status' => 'success', 'message' => 'You are registered'], 200);
+    }
+
+    public function loginapp(Request $request){
+
+        $admin = Admin::where('email', $request->input('email'))->first();
+
+        if(!$admin) {
+            return response()->json(['status' => 'error', 'message' => 'User not found'], 401);
+        }
+
+        $pass = $request->input('password');
+        if(password_verify ( $pass, $admin->password )){
+            $admin->api_token = Str::random(60);
+            $admin->save();
+            return response()->json(['status' => 'success', 'message' => 'Logged in succesfully'], 200);
+        }
+        return response()->json(['status' => 'error', 'message' => 'Incorrect password'], 401);
+    }
+
+    public function storeAbout(Request $request){
+        $admins = Admin::all();
+
+        foreach($admins as $admin){
+            $admin->about_us = $request->input('about_us');
+            $admin->about_us_img = $request->input('about_us_img');
+            $admin->save();
+        }
+        return response()->json(['status' => 'success', 'message' => 'Saved succesfully'], 200);
+    }
+
     public function aboutusapp(){
         $admin = Admin::first();
         return response()->json($admin);
